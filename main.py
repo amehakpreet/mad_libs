3f1db1ec4870886f01da56f7156612fd6593e0a5
amehakpreet <Mehakpreeta203@deltalearns.ca>
2025-06-13 10:25:45 -0700

    started it

---
+++ main.py
@@ -0,0 +1,11 @@
+# Start game
+# Ask users name and welcome them
+name = input("Welcome to Mad Libs. You will be asked to choose series of words and in the end they will be all put together to make a story. May I have your name? ")
+print(f"Welcome, {name}. Get ready to laugh!")
+
+# Start game
+print("")
+print("First, choose an adjective.(A decscribing word) ")
+word1 = input("Adjective = ").lower().strip()
+
+
